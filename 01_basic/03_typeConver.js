7a93e2e28a64f2fff2fc65483a0c5f5a0caea5db
Khushi Merai <144791385+Khushimerai@users.noreply.github.com>
2026-05-02 07:54:49 +0000

    type conversion

---
+++ 01_basic/03_typeConver.js
@@ -0,0 +1,22 @@
+let score = "33"
+
+let valueInNumber = Number(score)
+console.log(valueInNumber);
+
+
+let str = "123"
+
+let num1 = Number(str)
+let num2 = parseInt(str)
+let num3 = parseFloat(str)
+
+console.log(num1 , typeof num1)
+console.log(num2 , typeof num2)
+console.log(num3 , typeof num3)
+
+let bool1 = true
+let bool2 = false
+
+console.log(Number(bool1));
+console.log(Number(bool2));
+
